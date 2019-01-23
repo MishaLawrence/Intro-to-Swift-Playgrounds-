0236602d7ab348b4d59da7090f75594f27c1938e
misha <mishasl@hawaii.edu>
2019-01-22 22:25:41 -1000

    Completed Assignment #2
    Introduction to True or False Variables (Boolean variables)

--- SwiftPlaygroundOne.playground/Contents.swift
+++ SwiftPlaygroundOne.playground/Contents.swift
@@ -1,4 +1,4 @@
-//:Plauground - noun: a place where people can play
+//:Playground - noun: a place where people can play
 
 import UIKit
 
@@ -14,4 +14,24 @@ var mySecondVariable = "and look I added two strings together"
 //Let's add two strings together
 var myThirdVariable = myFirstVariable + mySecondVariable
 print(myThirdVariable)
+
+//Assignment #2 Introduction to True and False
+var appState = false
+//Declared boolean variable and print
+print(appState)
+//Comparing variables using > (greater than) or < (less than)
+var numberOne = 5
+var numberTwo = 7
+//Create avariable that holds the comparison on the two variables previously created
+var compare = numberOne>numberTwo
+
+
+
+
+
+
+
+
+
+
 
